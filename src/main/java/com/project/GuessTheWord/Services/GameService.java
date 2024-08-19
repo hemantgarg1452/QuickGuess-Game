9bdd0445b0dbf4d53727ca5bfa32d827eeffa9a2
Hemant Garg <hanygarg1452@gmail.com>
2024-08-19 16:55:45 +0530

    Update GameService.java

--- src/main/java/com/project/GuessTheWord/Services/GameService.java
+++ src/main/java/com/project/GuessTheWord/Services/GameService.java
@@ -5,12 +5,27 @@ import java.util.Random;
 
 @Service
 public class GameService {
-
+    private String randomChoosen = null;
     private String[] randomWords = {"Java", "twitter", "Game", "meta", "apartment", "flight", "japan"};
+    private char[] allCharacters;
     private Random random = new Random();
 
 
+    public GameService() {
+        randomChoosen = randomWords[random.nextInt(randomWords.length)];
+        System.out.println("random word is: "+ randomChoosen);
+        allCharacters = new char[randomChoosen.length()];
+    }
+
     public String getRandomWord() {
-        return randomWords[random.nextInt(randomWords.length)];
+        String blank = "";
+
+        for (char ch : allCharacters){
+            if(ch=='\u0000') blank+="__";
+
+            blank+=' ';
+        }
+        return blank;
+
     }
 }
